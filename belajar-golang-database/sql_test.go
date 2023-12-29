e334ffe27feaf509a4d02363ed4fa9c729998b42
iqmalakur <iakbarkurnia@gmail.com>
2023-12-29 22:26:16 +0700

    [belajar-golang-database] database transaction

--- belajar-golang-database/sql_test.go
+++ belajar-golang-database/sql_test.go
@@ -222,3 +222,40 @@ func TestPrepareStatement(t *testing.T) {
 		fmt.Println("Comment Id", id)
 	}
 }
+
+func TestTransaction(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	tx, err := db.Begin()
+	if err != nil {
+		panic(err)
+	}
+
+	script := "INSERT INTO comments (email, comment) VALUES (?, ?)"
+
+	for i := 0; i < 10; i++ {
+		email := "akur" + strconv.Itoa(i) + "@gmail.com"
+		comment := "Komentar ke " + strconv.Itoa(i)
+
+		result, err := tx.ExecContext(ctx, script, email, comment)
+		if err != nil {
+			panic(err)
+		}
+
+		id, err := result.LastInsertId()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Comment Id", id)
+	}
+
+	// err = tx.Rollback()
+	err = tx.Commit()
+	if err != nil {
+		panic(err)
+	}
+}
