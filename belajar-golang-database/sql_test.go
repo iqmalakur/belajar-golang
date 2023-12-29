c04f4d3f09a082a4ede78a4ca11a51cee15659c3
iqmalakur <iakbarkurnia@gmail.com>
2023-12-29 20:20:10 +0700

    [belajar-golang-database] get last insert id (auto increment)

--- belajar-golang-database/sql_test.go
+++ belajar-golang-database/sql_test.go
@@ -167,3 +167,26 @@ func TestExecSqlParameter(t *testing.T) {
 
 	fmt.Println("Success insert new user")
 }
+
+func TestAutoIncrement(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	email := "akur@gmail.com"
+	comment := "Test komen"
+
+	script := "INSERT INTO comments (email, comment) VALUES (?, ?)"
+	result, err := db.ExecContext(ctx, script, email, comment)
+	if err != nil {
+		panic(err)
+	}
+
+	insertId, err := result.LastInsertId()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Success insert new comment with id", insertId)
+}
