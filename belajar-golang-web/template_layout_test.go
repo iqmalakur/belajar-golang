5bc32f5922caaef73d04420061c44e3b7bc22690
iqmalakur <iakbarkurnia@gmail.com>
2024-01-13 16:22:54 +0700

    [belajar-golang-web] template layout

---
+++ belajar-golang-web/template_layout_test.go
@@ -0,0 +1,32 @@
+package belajar_golang_web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"text/template"
+)
+
+func TemplateLayout(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.ParseFiles("./templates/layout.gohtml", "./templates/header.gohtml", "./templates/footer.gohtml"))
+	t.ExecuteTemplate(writer, "layout", map[string]interface{}{
+		"Title": "Template Layout",
+		"Name":  "AKUR",
+	})
+}
+
+func TestTemplateLayout(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateLayout(recorder, request)
+
+	body, err := io.ReadAll(recorder.Result().Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(body))
+}
