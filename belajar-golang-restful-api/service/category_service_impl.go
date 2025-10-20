96afa67deada50dcdf93281cdd927bf19b595606
iqmalakur <iakbarkurnia@gmail.com>
2025-10-20 15:23:05 +0700

    feat: create category service implementation

---
+++ belajar-golang-restful-api/service/category_service_impl.go
@@ -0,0 +1,71 @@
+package service
+
+import (
+	"context"
+	"database/sql"
+	"iqmalakur/belajar-golang-restful-api/helper"
+	"iqmalakur/belajar-golang-restful-api/model/domain"
+	"iqmalakur/belajar-golang-restful-api/model/web"
+	"iqmalakur/belajar-golang-restful-api/repository"
+)
+
+type CategoryServiceImpl struct {
+	Repository repository.CategoryRepository
+	DB         *sql.DB
+}
+
+func (service *CategoryServiceImpl) Create(ctx context.Context, request web.CategoryCreateRequest) web.CategoryResponse {
+	tx, err := service.DB.Begin()
+	helper.PanicIfError(err)
+	defer helper.CommitOrRollback(tx)
+
+	category := domain.Category{Name: request.Name}
+	category = service.Repository.Save(ctx, tx, category)
+
+	return helper.ToCategoryResponse(category)
+}
+
+func (service *CategoryServiceImpl) Update(ctx context.Context, request web.CategoryUpdateRequest) web.CategoryResponse {
+	tx, err := service.DB.Begin()
+	helper.PanicIfError(err)
+	defer helper.CommitOrRollback(tx)
+
+	category, err := service.Repository.FindById(ctx, tx, request.Id)
+	helper.PanicIfError(err)
+
+	category.Name = request.Name
+	category = service.Repository.Update(ctx, tx, category)
+
+	return helper.ToCategoryResponse(category)
+}
+
+func (service *CategoryServiceImpl) Delete(ctx context.Context, id int) {
+	tx, err := service.DB.Begin()
+	helper.PanicIfError(err)
+	defer helper.CommitOrRollback(tx)
+
+	category, err := service.Repository.FindById(ctx, tx, id)
+	helper.PanicIfError(err)
+
+	service.Repository.Delete(ctx, tx, category)
+}
+
+func (service *CategoryServiceImpl) FindById(ctx context.Context, id int) web.CategoryResponse {
+	tx, err := service.DB.Begin()
+	helper.PanicIfError(err)
+	defer helper.CommitOrRollback(tx)
+
+	category, err := service.Repository.FindById(ctx, tx, id)
+	helper.PanicIfError(err)
+
+	return helper.ToCategoryResponse(category)
+}
+
+func (service *CategoryServiceImpl) FindAll(ctx context.Context) []web.CategoryResponse {
+	tx, err := service.DB.Begin()
+	helper.PanicIfError(err)
+	defer helper.CommitOrRollback(tx)
+
+	categories := service.Repository.FindAll(ctx, tx)
+	return helper.ToCategoryResponses(categories)
+}
