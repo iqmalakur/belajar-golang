3871dd562e4a80950562fe8b1f60a561aa52ecd5
iqmalakur <iakbarkurnia@gmail.com>
2025-10-24 15:40:22 +0700

    feat: create simple repository and service for provider

---
+++ belajar-golang-dependency-injection/simple/simple.go
@@ -0,0 +1,15 @@
+package simple
+
+type SimpleRepository struct{}
+
+func NewSimpleRepository() *SimpleRepository {
+	return &SimpleRepository{}
+}
+
+type SimpleService struct {
+	*SimpleRepository
+}
+
+func NewSimpleService(repository *SimpleRepository) *SimpleService {
+	return &SimpleService{SimpleRepository: repository}
+}
