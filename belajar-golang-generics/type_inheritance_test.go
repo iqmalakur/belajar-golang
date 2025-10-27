2e5f9f99ae2a8a67f33e8cc141b1194b365a6cea
iqmalakur <iakbarkurnia@gmail.com>
2025-10-27 17:26:06 +0700

    feat: using type parameter inheritance

---
+++ belajar-golang-generics/type_inheritance_test.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type Employee interface {
+	GetName() string
+}
+
+func GetName[T Employee](param T) string {
+	return param.GetName()
+}
+
+type Manager interface {
+	GetName() string
+	GetManagerName() string
+}
+
+type MyManager struct {
+	Name string
+}
+
+func (m *MyManager) GetName() string {
+	return m.Name
+}
+
+func (m *MyManager) GetManagerName() string {
+	return m.Name
+}
+
+type VicePresident interface {
+	GetName() string
+	GetVicePresidentName() string
+}
+
+type MyVicePresident struct {
+	Name string
+}
+
+func (m *MyVicePresident) GetName() string {
+	return m.Name
+}
+
+func (m *MyVicePresident) GetVicePresidentName() string {
+	return m.Name
+}
+
+func TestTypeInheritance(t *testing.T) {
+	assert.Equal(t, "Ucup", GetName[Manager](&MyManager{Name: "Ucup"}))
+	assert.Equal(t, "Ucup", GetName[VicePresident](&MyVicePresident{Name: "Ucup"}))
+}
