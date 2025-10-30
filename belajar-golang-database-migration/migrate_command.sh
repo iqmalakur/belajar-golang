b68ed9f9ffbffc103a65f738c92c6beaebcda681
iqmalakur <iakbarkurnia@gmail.com>
2025-10-30 16:32:29 +0700

    feat: running migration up

--- belajar-golang-database-migration/migrate_command.sh
+++ belajar-golang-database-migration/migrate_command.sh
@@ -3,3 +3,6 @@ go install -tags "postgres,mysql,mongodb" github.com/golang-migrate/migrate/v4/c
 
 # Create migration table
 migrate create -ext sql -dir db/migrations create_table_category
+
+# Migration up
+migrate -database "mysql://root:root@tcp(localhost:3306)/belajar_golang_database_migration" -path db/migrations up
