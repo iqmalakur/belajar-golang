8fbba8892e9ea0d33b729f2802d0bcf607924f98
iqmalakur <iakbarkurnia@gmail.com>
2025-10-31 07:53:27 +0700

    feat: validate two variables

--- belajar-golang-validation/validation_test.go
+++ belajar-golang-validation/validation_test.go
@@ -16,9 +16,23 @@ func TestValidation(t *testing.T) {
 
 func TestValidationVariable(t *testing.T) {
 	validate := validator.New()
-	var user string
+	// var user string
+	var user string = "usr"
 
 	err := validate.Var(user, "required")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+func TestValidateTwoVariables(t *testing.T) {
+	validate := validator.New()
+
+	password := "rahasia"
+	// confirmPassword := "salah"
+	confirmPassword := "rahasia"
+
+	err := validate.VarWithValue(password, confirmPassword, "eqfield")
 	if err != nil {
 		fmt.Println(err.Error())
 	}
