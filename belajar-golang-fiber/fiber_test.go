75c804a3867ab1506aebfbd6f6672c7446018cd5
iqmalakur <iakbarkurnia@gmail.com>
2025-11-23 17:08:29 +0700

    http response

--- belajar-golang-fiber/fiber_test.go
+++ belajar-golang-fiber/fiber_test.go
@@ -267,3 +267,24 @@ func TestBodyParserXML(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, "Register Success Ucup", string(bytes))
 }
+
+func TestResponseJSON(t *testing.T) {
+	app.Get("/user", func(ctx *fiber.Ctx) error {
+		return ctx.JSON(fiber.Map{
+			"username": "ucup",
+			"name":     "Ucup Surucup",
+		})
+	})
+
+	request := httptest.NewRequest(http.MethodGet, "/user", nil)
+	request.Header.Set("Accept", "application/json")
+	response, err := app.Test(request)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, err := io.ReadAll(response.Body)
+
+	assert.Nil(t, err)
+	assert.Equal(t, `{"name":"Ucup Surucup","username":"ucup"}`, string(bytes))
+}
