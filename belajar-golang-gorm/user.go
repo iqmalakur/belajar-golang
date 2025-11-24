5e39c3cea3bf62142f1278541e407f01a8fe3dff
iqmalakur <iakbarkurnia@gmail.com>
2025-11-24 14:03:05 +0700

    model

---
+++ belajar-golang-gorm/user.go
@@ -0,0 +1,21 @@
+package main
+
+import "time"
+
+// gorm automated mapping
+// User => users
+// OrderDetail => order_details
+// ID => Primary Key
+// or using gorm tag manually
+// add TableName method to manually set the table name
+type User struct {
+	ID         string    `gorm:"primary_key;column:id"`
+	Password   string    `gorm:"column:password"`
+	Name       string    `gorm:"column:name"`
+	CreatedAt  time.Time `gorm:"column:created_at;autoCreateTime"`
+	UpdateddAt time.Time `gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
+}
+
+func (u *User) TableName() string {
+	return "users"
+}
