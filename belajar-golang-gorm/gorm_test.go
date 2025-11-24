cb2c2823047b7f4a381072c915aa296c57b5604b
iqmalakur <iakbarkurnia@gmail.com>
2025-11-24 13:32:19 +0700

    add belajar golang gorm

---
+++ belajar-golang-gorm/gorm_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func OpenConnection() *gorm.DB {
+	dialect := mysql.Open("root:root@tcp(127.0.0.1:3306)/belajar_golang_gorm?charset=utf8mb4&parseTime=True&loc=Local")
+	db, err := gorm.Open(dialect, &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
+
+var db = OpenConnection()
+
+func TestOpenConnection(t *testing.T) {
+	assert.NotNil(t, db)
+}
