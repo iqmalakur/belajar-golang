883fe39bee6653cc1d83ba8db5f13c8a8575801c
iqmalakur <iakbarkurnia@gmail.com>
2025-11-25 14:41:19 +0700

    field permission

--- belajar-golang-gorm/user.go
+++ belajar-golang-gorm/user.go
@@ -9,11 +9,12 @@ import "time"
 // or using gorm tag manually
 // add TableName method to manually set the table name
 type User struct {
-	ID         string    `gorm:"primary_key;column:id"`
-	Password   string    `gorm:"column:password"`
-	Name       string    `gorm:"column:name"`
-	CreatedAt  time.Time `gorm:"column:created_at;autoCreateTime"`
-	UpdateddAt time.Time `gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
+	ID          string    `gorm:"primary_key;column:id;<-:create"`
+	Password    string    `gorm:"column:password"`
+	Name        string    `gorm:"column:name"`
+	CreatedAt   time.Time `gorm:"column:created_at;autoCreateTime;<-:create"`
+	UpdateddAt  time.Time `gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
+	Information string    `gorm:"-"`
 }
 
 func (u *User) TableName() string {
